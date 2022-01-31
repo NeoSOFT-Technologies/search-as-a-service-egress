71390542bb493b12178ccde37dc3609457b59f79
kirti.mandwade.neosoft <kirti.mandwade.neosoft@hopscotch.in>
2022-01-31 17:46:36 +0530

    full application update

---
+++ src/main/java/com/searchservice/app/domain/dto/SolrSearchResponseDTO.java
@@ -0,0 +1,29 @@
+package com.searchservice.app.domain.dto;
+
+import org.springframework.stereotype.Component;
+
+import com.searchservice.app.infrastructure.adaptor.SolrSearchResult;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Component
+public class SolrSearchResponseDTO {
+	private int statusCode;
+	private String responseMessage;
+	private SolrSearchResult results;
+	
+	public SolrSearchResponseDTO(String responseMessage, SolrSearchResult solrSearchResultResponse) {
+		this.responseMessage = responseMessage;
+		this.results = solrSearchResultResponse;
+	}
+
+	@Override
+	public String toString() {
+		return "SolrSearchResponseDTO [statusCode=" + statusCode + ", "
+						+ "responseMessage=" + responseMessage + ", "
+						+ "solrSearchResultResponse=" + results + "]";
+	}
+}
---
+++ src/main/java/com/searchservice/app/domain/service/SolrSearchAdvanced.java
@@ -0,0 +1,40 @@
+package com.searchservice.app.domain.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.searchservice.app.domain.dto.SolrSearchResponseDTO;
+import com.searchservice.app.domain.port.api.SolrSearchRecordsServicePort;
+
+@Service
+@Transactional
+public class SolrSearchAdvanced {
+	private final Logger logger = LoggerFactory.getLogger(SolrSearchAdvanced.class);
+	
+	private SolrSearchRecordsServicePort solrSearchRecordsServicePort;
+	public SolrSearchAdvanced(SolrSearchRecordsServicePort solrSearchRecordsServicePort) {
+		this.solrSearchRecordsServicePort = solrSearchRecordsServicePort;
+	}
+	
+	public SolrSearchResponseDTO search(
+			String tableName, 
+			String queryField, 
+			String queryFieldSearchTerm, 
+			String startRecord, 
+			String pageSize, 
+			String sortTag, 
+			String sortOrder
+			) {
+		logger.debug("Advanced search for the given table");
+		return solrSearchRecordsServicePort.setUpSelectQueryAdvancedSearch(
+				tableName, 
+				queryField, 
+				queryFieldSearchTerm, 
+				startRecord, 
+				pageSize, 
+				sortTag, 
+				sortOrder);
+	}
+}
