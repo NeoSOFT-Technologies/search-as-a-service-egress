d8132dfcad8f4ca910c71a82ae95d6ada90326dc
shubhamphepale <shubhamphepale@gmail.com>
2022-02-15 12:51:18 +0530

    Implement logger in egress

--- src/main/java/com/searchservice/app/rest/SearchResource.java
+++ src/main/java/com/searchservice/app/rest/SearchResource.java
@@ -1,5 +1,8 @@
 package com.searchservice.app.rest;
 
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.searchservice.app.domain.dto.SolrSearchResponseDTO;
+import com.searchservice.app.domain.dto.logger.LoggersDTO;
 import com.searchservice.app.domain.service.SolrSearchAdvanced;
+import com.searchservice.app.domain.utils.LoggerUtils;
 import com.searchservice.app.infrastructure.adaptor.SolrSearchResult;
 
 @RestController
@@ -20,6 +25,12 @@ import com.searchservice.app.infrastructure.adaptor.SolrSearchResult;
 public class SearchResource {
     /* Solr Search Records for given collection- Egress Service Resource */
     private final Logger logger = LoggerFactory.getLogger(SearchResource.class);
+    
+    ZonedDateTime utc = ZonedDateTime.now(ZoneOffset.UTC);
+    
+    private String servicename = "Search_Resource";
+    
+    private String username = "Username";  
 
     private SolrSearchAdvanced solrSearchAdvanced;
 
@@ -33,18 +44,39 @@ public class SearchResource {
     @Autowired
     SolrSearchResult solrSearchResult;
 
+    private void successMethod(String nameofCurrMethod, LoggersDTO loggersDTO) {
+		String timestamp;
+		loggersDTO.setServicename(servicename);
+		loggersDTO.setUsername(username);
+		loggersDTO.setNameofmethod(nameofCurrMethod);
+		timestamp = LoggerUtils.utcTime().toString();
+		loggersDTO.setTimestamp(timestamp);
+	}
+    
     @GetMapping(value = "/v1/{clientId}/{tableName}")
     public ResponseEntity<SolrSearchResponseDTO> searchRecordsInGivenCollectionAdvanced(@PathVariable int clientId, @PathVariable String tableName,
             @RequestParam(defaultValue = "name") String queryField, @RequestParam(defaultValue = "*") String searchTerm, @RequestParam(defaultValue = "0") String startRecord,
             @RequestParam(defaultValue = "5") String pageSize, @RequestParam(defaultValue = "id") String orderBy, @RequestParam(defaultValue = "asc") String order) {
         logger.debug("REST call for ADVANCED SEARCH search in the given collection");
+
+        String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		String timestamp = LoggerUtils.utcTime().toString();
+		LoggersDTO loggersDTO = LoggerUtils.getRequestLoggingInfo(servicename, username,nameofCurrMethod,timestamp);
+		LoggerUtils.printlogger(loggersDTO,true,false);
+		loggersDTO.setCorrelationid(loggersDTO.getCorrelationid());
+		loggersDTO.setIpaddress(loggersDTO.getIpaddress());
+		
         tableName = tableName + "_" + clientId;
-        SolrSearchResponseDTO solrSearchResponseDTO = solrSearchAdvanced.search(tableName, queryField, searchTerm, startRecord, pageSize, orderBy, order);
+        SolrSearchResponseDTO solrSearchResponseDTO = solrSearchAdvanced.search(tableName, queryField, searchTerm, startRecord, pageSize, orderBy, order,loggersDTO);
+
+        successMethod(nameofCurrMethod, loggersDTO);
+		
         if (solrSearchResponseDTO.getStatusCode() == 200) {
+        	LoggerUtils.printlogger(loggersDTO,false,false);
             return ResponseEntity.status(HttpStatus.OK).body(solrSearchResponseDTO);
         } else {
+        	LoggerUtils.printlogger(loggersDTO,false,true);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(solrSearchResponseDTO);
         }
     }
-
 }
